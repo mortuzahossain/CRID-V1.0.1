43e5688da3d4d9570eabb36773b53efba3a0b758
Mortuza Hossain <mortuzahossain1997@gmail.com>
2019-09-09 09:59:12 +0600

    Update User Data Done

--- user/update.php
+++ user/update.php
@@ -4,6 +4,14 @@
         header("Location: ../index.php");
     }
 
+	$userid =$_SESSION['id'];
+
+    function xss_cleaner($input_str) {
+        $return_str = str_replace( array('<',';','|','&','>',"'",'"',')','('), array('&lt;','&#58;','&#124;','&#38;','&gt;','&apos;','&#x22;','&#x29;','&#x28;'), $input_str );
+        $return_str = str_ireplace( '%3Cscript', '', $return_str );
+        return htmlspecialchars($return_str);
+    }
+
 	$PAGENAME = "Update Users Profile";
 	include $_SERVER["DOCUMENT_ROOT"].'/inc/header.php';
 	include $_SERVER["DOCUMENT_ROOT"].'/inc/navbar.php';
@@ -12,6 +20,24 @@
 
 	<section class="container about-section">
 		<div class="gap"></div>
+
+<?php
+	if (isset($_POST['update'])) {
+		$name = xss_cleaner($_POST['name']);
+		$gender = xss_cleaner($_POST['gender']);
+		$account_type = xss_cleaner($_POST['account_type']);
+
+		$update_query = "UPDATE users set name = '$name',gender = '$gender',account_type = '$account_type' WHERE id = $userid";
+
+		if (mysqli_query($con,$update_query)) {
+			echo "<div class='alert alert-success' role='alert'>User information updated sucessfully.</div>";
+		} else {
+			echo "<div class='alert alert-warning' role='alert'>We are fetching problem in updating your information.</div>";
+		}
+	}
+
+?>
+
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2">
 				<ul class="nav nav-tabs">
@@ -25,22 +51,17 @@
 			</div>
 		</div>
 <?php
-$userid =$_SESSION['id'];
-$sql = "SELECT * FROM users WHERE id = $userid";
+$sql = "SELECT name FROM users WHERE id = $userid";
 $result = mysqli_query($con,$sql)->fetch_assoc();
 ?>
 
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2">
 				<h2 class="text-center">Update Your Profile</h2>
-				<form>
+				<form method="post" accept="">
 				  <div class="form-group">
 				    <label for="Name">Name</label>
-				    <input type="text" class="form-control" id="Name" placeholder="" value="<?php echo $result['name']; ?>">
-				  </div>
-				  <div class="form-group">
-				    <label for="exampleInputEmail1">Email address</label>
-				    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="" value="<?php echo $result['email']; ?>">
+				    <input type="text" class="form-control" id="Name" placeholder="" name="name"value="<?php echo $result['name']; ?>">
 				  </div>
 				  <div class="form-group">
 	                <label for="gender">Gender</label><br>
@@ -55,7 +76,7 @@ $result = mysqli_query($con,$sql)->fetch_assoc();
 	                      <option value="Mentor/Trainer">Mentor/Trainer</option>
 	                  </select>
 	              </div>
-				  <button type="submit" class="btn btn-primary">Update</button>
+				  <button type="submit" class="btn btn-primary" name="update">Update</button>
 				</form>
 			</div>
 		</div>
